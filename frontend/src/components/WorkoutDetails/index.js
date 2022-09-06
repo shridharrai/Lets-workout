f06ab30be3f205c4b0186a333544693fba5db53e
shridharrai <shridharrai3@gmail.com>
2022-09-06 17:29:30 +0530

    feat: Date formatted by library and delete icon added

--- frontend/src/components/WorkoutDetails/index.js
+++ frontend/src/components/WorkoutDetails/index.js
@@ -1,5 +1,6 @@
 import { type } from "../../context/constants";
 import { useWorkoutContext } from "../../hooks/WorkoutContext";
+import formatDistanceToNow from "date-fns/formatDistanceToNow";
 import "./index.css";
 
 const WorkoutDetails = ({ workout }) => {
@@ -27,8 +28,12 @@ const WorkoutDetails = ({ workout }) => {
         <strong>Reps: </strong>
         {workout.reps}
       </p>
-      <p>{workout.createdAt}</p>
-      <span onClick={handleClick}>delete</span>
+      <p>
+        {formatDistanceToNow(new Date(workout.createdAt), { addSuffix: true })}
+      </p>
+      <span className="material-symbols-outlined" onClick={handleClick}>
+        delete
+      </span>
     </div>
   );
 };
